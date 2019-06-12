195bdb03df8e047980e6d228c7969153954ac77c
huanhuan <18232827839@163.com>
2019-06-12 14:54:35 +0800

    init

---
+++ build/webpack.config.base.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+const isDev = process.env.NODE_ENV === "development";
+
+const config = {
+  target: "web",
+  entry: path.join(__dirname, "./src/index.js"), //入口
+  output: {
+    //出口
+    filename: "bundle.[hash:8]js", //出口文件名
+    path: path.join(__dirname, "dist") //出口文件打包的文件
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, loader: "vue-loader" },
+      { test: /\.jsx$/, loader: "babel-loader" },
+      { test: /\.js$/, loader: "babel-loader",exclude:/node_modules/ },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 1024,
+              name: "[name]-huanhuan.[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+module.exports = config;
